cd8e1687edfd76a1844a7ce72f4556f3484f5aa9
Toolfashion <34125985+Toolfashion@users.noreply.github.com>
2026-04-23 07:34:36 +0200

    Update index.js

--- frontend/src/translations/index.js
+++ frontend/src/translations/index.js
@@ -2032,8 +2032,7 @@ export const translations = {
     // Reels
     watchReel: "Sledovat Reel",
   },
-};
-
+  };
 // Language names for the selector
 export const languageNames = {
   de: "Deutsch",
